727d89656f0d702574c02c67cc5e1a48ce484e1c
Lucas Reis <66570455+lucasreisc@users.noreply.github.com>
2024-08-18 13:34:37 -0300

    update
    uptade

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,5 +1,5 @@
 from modelos.avaliacao import Avaliacao
-from modelos.cardapio.item_cardapio import ItemCardapio 
+from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Restaurante:
     restaurantes = []
@@ -19,7 +19,7 @@ class Restaurante:
     def listar_restaurantes(cls):
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} |{'Status'}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str (restaurante.media_avaliacoes).ljust(25)} |{restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} |{restaurante.ativo}')
 
     @property
     def ativo(self):
@@ -29,7 +29,7 @@ class Restaurante:
         self._ativo = not self._ativo
 
     def receber_avaliacao(self, cliente, nota):
-        if 0 < nota <= 5 :
+        if 0 < nota <= 5: 
             avaliacao = Avaliacao(cliente, nota)
             self._avaliacao.append(avaliacao)
 
@@ -39,26 +39,20 @@ class Restaurante:
             return '-'
         soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
         quantidade_de_notas = len(self._avaliacao)
-        media = round (soma_das_notas / quantidade_de_notas ,1)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
         return media
 
-   # def adicionar_bebida_no_cardapio(self, bebida):
-    #    self._cardapio.append(bebida)
-
-    #def adicionar_prato_no_cardapio(self,prato): 
-     #   self._cardapio.append(prato) 
-
     def adicionar_no_cardapio(self,item):
-        if isinstance(item, ItemCardapio):
+        if isinstance(item,ItemCardapio):
             self._cardapio.append(item)
     
     @property
     def exibir_cardapio(self):
         print(f'Cardapio do restaurante {self._nome}\n')
-        for i, item in enumerate(self._cardapio, start=1):
+        for i,item in enumerate(self._cardapio,start=1):
             if hasattr(item,'descricao'):
-                mensagem_prato = f'{i}.Nome:{item._nome} | Preço: R${item._preco} | Descrição: {item.descricao}'
+                mensagem_prato = f'{i}. Nome: {item._nome} | Preço: R${item._preco} | Descrição: {item.descricao}'
                 print(mensagem_prato)
             else:
-                mensagem_bebida = f'{i}.Nome:{item._nome} | Preço: R${item._preco} | Tamanho: {item.tamanho}'
+                mensagem_bebida = f'{i}. Nome: {item._nome} | Preço: R${item._preco} | Tamanho: {item.tamanho}'
                 print(mensagem_bebida)
